d0fbe6f80591b40e1d37deab896f7864c80d73ce
Alexander_Ovchar <azalexxx02@gmail.com>
2023-11-18 01:36:58 +0200

    new start react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { EarthScene } from "./components";
+
+
+function App() {
+  return (
+    <div className="App">
+      <EarthScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,1 @@
+export { EarthScene } from "./EarthScene";
