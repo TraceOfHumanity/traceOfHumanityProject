cb61ade82ceaba3b31851956245ace6abcbfba72
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-17 00:29:07 +0300

    fix error r3d

--- src/pages/AboutAuthor.tsx
+++ src/pages/AboutAuthor.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { AuthorBG } from "components/AuthorPageComponents/Background";
 import { AuthorContent } from "components/AuthorPageComponents/Content";
+import { AuthorBG } from "components/AuthorPageComponents/Background";
 
 export const AboutAuthor = () => {
   return (
     <div>
-      {/* <AuthorBG /> */}
+      <AuthorBG />
       <AuthorContent />
     </div>
   );
