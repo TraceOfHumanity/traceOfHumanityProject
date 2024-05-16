f0d0d41ac924ecac92c805c547959421c20848d1
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-16 23:30:29 +0300

    fix main menu items

--- src/components/MainMenu/MenuItemWrapper.tsx
+++ src/components/MainMenu/MenuItemWrapper.tsx
@@ -4,7 +4,7 @@ import React, {useEffect, useRef} from "react";
 
 interface MenuItemWrapperProps {
   children: React.ReactNode;
-  buttonId: string | boolean;
+  buttonId?: string;
 }
 
 export const MenuItemWrapper: React.FC<MenuItemWrapperProps> = ({
@@ -41,7 +41,7 @@ export const MenuItemWrapper: React.FC<MenuItemWrapperProps> = ({
 
   return (
     <div
-      className="odd:bg absolute flex w-fit min-w-44 max-w-44 transform items-center gap-2 border-b border-t border-b-opacityBlue border-t-opacityRed bg-gradient-to-l from-opacityBlue01 to-opacityRed01 px-2 py-1 text-xs font-semibold uppercase drop-shadow-xl backdrop-blur-sm md:text-sm lg:py-2 2xl:text-base"
+      className="odd:bg absolute flex w-fit min-w-44 max-w-44 transform items-center gap-2 border-b border-t border-b-opacityBlue border-t-opacityRed bg-gradient-to-l from-opacityBlue01 to-opacityRed01 px-2 py-1 text-xs font-semibold uppercase drop-shadow-xl backdrop-blur-sm md:text-sm lg:py-2 2xl:text-base [&_*]:uppercase"
       ref={itemRef}
     >
       {children}
--- src/components/MainMenu/Navigation.tsx
+++ src/components/MainMenu/Navigation.tsx
@@ -1,26 +1,28 @@
-import { useAppNavigation } from "hooks/useAppNavigation";
-import React, { useRef } from "react";
-import { MdSpaceDashboard } from "react-icons/md";
-import { Link } from "react-router-dom";
+import {useAppNavigation} from "hooks/useAppNavigation";
+import React from "react";
+import {MdSpaceDashboard} from "react-icons/md";
+import {Link} from "react-router-dom";
 
-import { MenuItemWrapper } from "./MenuItemWrapper";
-import { useAppSelector } from "hooks/useReduxToolkit";
+import {MenuItemWrapper} from "./MenuItemWrapper";
+import {useAppSelector} from "hooks/useReduxToolkit";
 
 export const Navigation = () => {
-  const { mainMenuItems } = useAppNavigation();
-  const { userId } = useAppSelector((state) => state.auth);
+  const {mainMenuItems} = useAppNavigation();
+  const {userId} = useAppSelector((state) => state.auth);
 
   return (
     <>
       {mainMenuItems.map((item, index) => (
         <MenuItemWrapper
           key={index}
-          buttonId={item.name === "login" && "loginButton"}
+          buttonId={item.name === "login" ? "loginButton" : `${item.name}`}
         >
           {item.icon}
-          <Link to={item.link} onClick={item.onClick}>
-            {item.name}
-          </Link>
+          {item.link ? (
+            <Link to={item.link}>{item.name}</Link>
+          ) : (
+            <button onClick={item.onClick}>{item.name}</button>
+          )}
         </MenuItemWrapper>
       ))}
       {userId === process.env.REACT_APP_TRACE_OF_HUMANITY && (
