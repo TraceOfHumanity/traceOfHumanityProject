7dcf83f8d579c182117ef3ddee92e4e5c6847fd5
Alexander_Ovchar <azalexxx02@gmail.com>
2024-06-23 19:32:54 +0300

    add deletePost, withdrawTheOfferToCreateAnArticle functions

---
+++ src/components/Popups/DeletePostConfirmationPopup.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DeletePostconfirmationPopup = () => {
+  return (
+    <div>DeletePostconfirmationPopup</div>
+  )
+}
--- src/components/PostActions.tsx
+++ src/components/PostActions.tsx
@@ -3,17 +3,51 @@ import {FaCheck} from "react-icons/fa";
 import {MdDeleteForever} from "react-icons/md";
 
 import {useAppNavigation} from "hooks/useAppNavigation";
+import {useFirebase} from "hooks/useFirebase";
 
 export const PostActions = () => {
   const {getPath} = useAppNavigation();
-  const {pathArray} = getPath();
+  const {
+    deletePost,
+    withdrawTheOfferToCreateAnArticle,
+    acceptArticleCreationRequests,
+  } = useFirebase();
+  const postId = getPath().pathArray[getPath().pathArray.length - 1];
+  console.log(postId);
+
+  const handleDelete = () => {
+    if (getPath().pathArray.includes("dashboard")) {
+      withdrawTheOfferToCreateAnArticle(postId)
+        .then(() => console.log("Offer is withdrawn successfully"))
+        .catch((error) => console.error(error));
+    }
+    if (getPath().pathArray.includes("library")) {
+      deletePost(postId)
+        .then(() => console.log("Post is deleted successfully"))
+        .catch((error) => console.error(error));
+    }
+  };
+
+  const handleAccept = () => {
+    acceptArticleCreationRequests(postId)
+      .then(() =>
+        console.log("Article creation request is accepted successfully"),
+      )
+      .catch((error) => console.error(error));
+  };
 
   return (
     <div className="absolute right-0 top-0 flex gap-2 opacity-0 group-hover:opacity-100">
-      <button className="rounded border border-borderColor p-1">
+      <button
+        className="rounded border border-borderColor p-1"
+        onClick={handleAccept}
+      >
         <FaCheck />
       </button>
-      <button className="rounded border border-borderColor p-1">
+      <button
+        className="rounded border border-borderColor p-1"
+        onClick={handleDelete}
+      >
         <MdDeleteForever />
       </button>
     </div>
